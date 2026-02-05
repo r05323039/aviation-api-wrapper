1f444bd2c548880a381ed9c2ec78b29ee779267b
ian.lin <ian.lin@webcomm.com.tw>
2026-02-05 14:20:52 +0800

    add handleBusinessException

--- src/main/java/com/opennet/assignment/aviation/exception/GlobalExceptionHandler.java
+++ src/main/java/com/opennet/assignment/aviation/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.opennet.assignment.aviation.exception;
 
 import com.opennet.assignment.aviation.dto.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,6 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     private ResponseEntity<ApiResponse<Void>> buildResponse(ErrorCode errorCode) {
         return new ResponseEntity<>(
@@ -32,6 +34,11 @@ public class GlobalExceptionHandler {
                 ApiResponse.error(ErrorCode.INVALID_INPUT, errors),
                 ErrorCode.INVALID_INPUT.getHttpStatus()
         );
+    }
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ApiResponse<Void>> handleBusinessException(BusinessException e) {
+        log.warn("Business Exception: {}", e.getErrorCode().getMessage());
+        return buildResponse(e.getErrorCode());
     }
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleAll(Exception e) {
